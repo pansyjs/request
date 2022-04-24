fdcd1006a38ed8b068d1004d204c86da0f663280
wangxingkang <wang_xingkang@qq.com>
2022-04-24 21:51:39 +0800

    ✨ feat: 优化实现

--- src/request.ts
+++ src/request.ts
@@ -1,60 +1,116 @@
 import axios from 'axios';
 
-import type { AxiosInstance, } from 'axios';
+import type { AxiosInstance } from 'axios';
 import type {
   Request,
-  RequestError,
+  RequestOptions,
   RequestConfig,
 } from './types';
 
 let requestInstance: AxiosInstance;
 let config: RequestConfig = {};
 
+/**
+ * 设置配置数据
+ * @param data
+ */
+export const setConfig = (data: RequestConfig = {}) => {
+  config = data;
+}
+
+/**
+ * 获取请求实例
+ * @returns
+ */
 const getRequestInstance = (): AxiosInstance => {
   if (requestInstance) return requestInstance;
 
   requestInstance = axios.create(config);
 
+  config?.requestInterceptors?.forEach((interceptor) => {
+    return interceptor instanceof Array ?
+      requestInstance.interceptors.request.use(interceptor[0], interceptor[1]):
+      requestInstance.interceptors.request.use(interceptor);
+    });
+
+  config?.responseInterceptors?.forEach((interceptor) => {
+    return interceptor instanceof Array ?
+      requestInstance.interceptors.response.use(interceptor[0], interceptor[1]):
+      requestInstance.interceptors.response.use(interceptor);
+    });
+
+  // 当响应的数据 success 是 false 的时候，抛出 error 以供 errorHandler 处理。
   requestInstance.interceptors.response.use((response)=>{
-    const {data} = response;
-    const adaptor = config?.errorConfig?.adaptor || ((resData) => resData);
-    const errorInfo = adaptor(data,response);
-    if(errorInfo.success === false){
-      const error: RequestError = new Error(errorInfo.errorMessage);
-      error.name = 'BizError';
-      error.data = data;
-      error.info = errorInfo;
-      throw error;
+    const { data } = response;
+
+    if (config?.errorConfig?.errorThrower) {
+      config.errorConfig.errorThrower(data);
     }
+
     return response;
   });
 
   return requestInstance;
 }
 
-export const setConfig = (data: RequestConfig = {}) => {
-  config = data;
-}
-
-export const request: Request = (url, opts) => {
+/**
+ * 请求方法
+ * @param url API地址
+ * @param opts axios 的配置对象
+ * @returns
+ */
+export const request: Request = (
+  url: string,
+  opts: RequestOptions = { method: 'GET' },
+) => {
   const requestInstance = getRequestInstance();
 
-  return new Promise((resolve, reject) => {
+  // @ts-ignore
+  const { getResponse = false, requestInterceptors, responseInterceptors } = opts;
+
+  const requestInterceptorsToEject = requestInterceptors?.map((interceptor) => {
+    return interceptor instanceof Array ?
+      requestInstance.interceptors.request.use(interceptor[0], interceptor[1]):
+      requestInstance.interceptors.request.use(interceptor);
+    });
+
+  const responseInterceptorsToEject = responseInterceptors?.map((interceptor) => {
+    return interceptor instanceof Array ?
+      requestInstance.interceptors.response.use(interceptor[0], interceptor[1]):
+      requestInstance.interceptors.response.use(interceptor);
+    });
+
+  return new Promise<any>((resolve, reject) => {
     requestInstance
       .request({ ...opts, url })
       .then((res) => {
-        const formatResultAdaptor =
-          config?.formatResultAdaptor || ((res) => res.data);
-        resolve(formatResultAdaptor(res));
+        requestInterceptorsToEject?.forEach((interceptor) => {
+          requestInstance.interceptors.request.eject(interceptor);
+        });
+        responseInterceptorsToEject?.forEach((interceptor) => {
+          requestInstance.interceptors.response.eject(interceptor);
+        });
+        resolve(getResponse ? res : res.data);
       })
       .catch((error) => {
+        requestInterceptorsToEject?.forEach((interceptor) => {
+          requestInstance.interceptors.request.eject(interceptor);
+        });
+        responseInterceptorsToEject?.forEach((interceptor) => {
+          requestInstance.interceptors.response.eject(interceptor);
+        });
+
         try {
           const handler = config.errorConfig?.errorHandler;
 
-          handler?.(error, opts, config);
+          if (handler) {
+            handler(error, opts, config);
+          }
         } catch (e) {
           reject(e);
         }
+
+        reject(error);
       });
   });
 }
--- src/types.ts
+++ src/types.ts
@@ -1,54 +1,81 @@
 import { ErrorShowType } from './config';
 
-import type {
-  AxiosResponse,
-  AxiosRequestConfig,
-} from 'axios';
+import type { AxiosResponse, AxiosRequestConfig } from 'axios';
 
-export interface RequestData<D = any> {
-  /** 状态码 */
+/** 后端返回数据格式 */
+export interface ResponseData<D = any> {
+  /** 接口状态码 */
   code: number;
-  /** 数据 */
+  /** 接口返回数据 */
   data: D,
-  /** 报错信息 */
+  /** 接口报错信息 */
   message: string;
-  /** 错误信息类型 */
-  showType?: ErrorShowType;
   [key: string]: any;
 }
 
-interface Adaptor {
-  (resData: any, response: AxiosResponse): RequestData;
-}
+type RequestInterceptor = (config : AxiosRequestConfig) => AxiosRequestConfig;
+type ResponseInterceptor = <T = any>(response : AxiosResponse<T>) => AxiosResponse<T> ;
 
-export interface FormatResultAdaptor {
-  (res: AxiosResponse): any;
-}
+type ErrorInterceptor = (error: Error) => Promise<Error>;
+
+type RequestInterceptorTuple = [RequestInterceptor, ErrorInterceptor] | [ RequestInterceptor ] | RequestInterceptor;
+type ResponseInterceptorTuple = [ResponseInterceptor, ErrorInterceptor] | [ResponseInterceptor] | ResponseInterceptor;
 
 export interface RequestError extends Error {
   data?: any;
-  info?: RequestData;
+  info?: ResponseData;
+  request?: any;
+  response?: any;
 }
 
-interface ErrorHandler {
-  (error: RequestError, opts: AxiosRequestConfig & { skipErrorHandler?: boolean }, config: RequestConfig): void;
+export interface ErrorHandler {
+  (error: RequestError, opts: RequestOptions, config: RequestConfig): void;
 }
 
-export interface RequestConfig extends AxiosRequestConfig {
+export interface RequestConfig<D = any> extends AxiosRequestConfig<D> {
+  /** 异常处理相关配置 */
   errorConfig?: {
-    errorPage?: string;
-    // adaptor 用以用户将不满足接口的后端数据修改成 errorInfo
-    adaptor?: Adaptor;
+    /**
+     * 该方法接收两个参数，第一个参数是 catch 到的 error，第二个参数则是 request 的 opts。
+     */
     errorHandler?: ErrorHandler;
-    defaultNoneResponseErrorMessage?: string;
-    defaultRequestErrorMessage?: string;
+    /**
+     * 接收后端返回的数据并且抛出一个自己的 error，可以在这里根据后端的数据进行一定的处理。
+     */
+    errorThrower?: <T = any>(res: T) => void
   };
-  formatResultAdaptor?: FormatResultAdaptor;
+  /** 请求拦截器 */
+  requestInterceptors?: RequestInterceptorTuple[];
+  /** 响应拦截器 */
+  responseInterceptors?: ResponseInterceptorTuple[];
 }
+
+export interface RequestOptions<D = any> extends AxiosRequestConfig<D> {
+  /**
+   * 是否跳过异常处理
+   * @default false
+   */
+  skipErrorHandler?: boolean;
+  /** 请求拦截器 */
+  requestInterceptors?: RequestInterceptorTuple[];
+  /** 响应拦截器 */
+  responseInterceptors?: ResponseInterceptorTuple[];
+}
+
+export interface RequestOptionsWithResponse<D = any> extends RequestOptions<D> {
+  getResponse: true;
+}
+
+export interface RequestOptionsWithoutResponse<D = any> extends RequestOptions<D> {
+  getResponse: false;
+}
+
 
 export interface Request {
-  (
-    url: string,
-    opts: AxiosRequestConfig & { skipErrorHandler?: boolean },
-  ): Promise<AxiosResponse<any, any>>;
+  <T = any>(url: string, opts: RequestOptionsWithResponse): Promise<AxiosResponse<T>>;
+  <T = any>(url: string, opts: RequestOptionsWithoutResponse): Promise<T>;
+  // getResponse 默认是 false， 因此不提供该参数时，只返回 data
+  <T = any>(url: string, opts: RequestOptions): Promise<T>;
+  // 不提供 opts 时，默认使用 'GET' method，并且默认返回 data
+  <T = any>(url: string): Promise<T>;
 }
