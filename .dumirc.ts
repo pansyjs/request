d582794ca741f356bc2e20499e013055f3711a51
wangxingkang <wang_xingkang@qq.com>
2022-11-18 12:02:19 +0800

    chore: upgrade dumi2

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  favicons: [
+    'https://gw.alipayobjects.com/zos/bmw-prod/b874caa9-4458-412a-9ac6-a61486180a62.svg'
+  ],
   themeConfig: {
     name: 'Request',
     logo: 'https://gw.alipayobjects.com/zos/bmw-prod/b874caa9-4458-412a-9ac6-a61486180a62.svg',
