3ac50b990c5893ef4837b89c590ecf5e93925a9b
wangxingkang <wang_xingkang@qq.com>
2024-04-10 15:30:11 +0800

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
+  globalName: 'request',
   format: ['cjs', 'esm', 'iife'],
   clean: true,
   legacyOutput: true,
